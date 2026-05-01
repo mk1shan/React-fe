585be238bee6bdad270a1f4e679bdc4080d7d51b
Mudipa Kishan <mudipakishanimayanga@gmail.com>
2026-05-01 10:47:36 +0530

    resgiterpage ui completed

--- src/features/auth/RegisterPage.tsx
+++ src/features/auth/RegisterPage.tsx
@@ -1,9 +1,97 @@
+import { useState } from "react";
+import { authService } from "./authService";
+import {
+  Alert,
+  Box,
+  Button,
+  Container,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+
 function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setLoading(true);
+    setSuccessMessage("");
+    setErrorMessage("");
+
+    try {
+      await authService.register({
+        name,
+        email,
+        password,
+      });
+
+      setSuccessMessage("Registration succesfull. you can now login");
+      setName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      setErrorMessage("Registration failed.please try again");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
- <h1>
-    Register page
- </h1>
-  )
+    <Container maxWidth="sm">
+      <Paper elevation={3} sx={{ padding: 4, marginTop: 8 }}>
+        <Typography variant="h4" align="center" gutterBottom>
+          create Account
+        </Typography>
+
+
+        {successMessage && <Alert severity="success">{successMessage}</Alert>}
+        {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+        <Box component="form" onSubmit={handleSubmit} sx={{ marginTop: 3 }}>
+          <TextField
+            fullWidth
+            label="Name"
+            margin="normal"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+
+          <TextField
+            fullWidth
+            label="Name"
+            margin="normal"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+
+          <TextField
+            fullWidth
+            label="Name"
+            margin="normal"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+
+          <Button
+            fullWidth
+            type="submit"
+            variant="contained"
+            disabled={loading}
+            sx={{ marginTop: 3 }}
+          >
+            {loading ? "Registering..." : "Register"}
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
